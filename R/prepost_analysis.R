c838cd50fca6333e1c65667fe717a629e9e95ca9
vvandewa <vincent.vandewalle@univ-lille.fr>
2020-07-30 01:31:09 +0200

    Update of pre-posterior analysis, adding the re-sampling of p by taking into account mu and s2

--- R/prepost_analysis.R
+++ R/prepost_analysis.R
@@ -30,18 +30,31 @@ sample_pm <- function(joint_pm, size) {
 #' @export prepost_pl
 #'
 
-prepost_pl <- function(m, pl, n, seed = NULL) {
+prepost_pl <- function(m, pl, mu_s2, n, seed = NULL) {
   if(!is.null(seed)) set.seed(seed)
   sapply(m, function(x){
     pl_m <- pl[[m+1]]
+    mu_s2_m <- mu_s2[[m+1]]
     if(is.null(pl_m)) {
       return(n*0)
     } else {
-      pl_m_sample <- pl_m[sample(x = 1:nrow(pl_m), size = 1),]
+      id = sample(x = 1:nrow(pl_m), size = 1)
+      pl_m_sample <- pl_m[id,]
+      mu_s2_sample <- mu_s2_m[id,]
       undiscovered <- lapply(n, function(x) {
         rbinom(length(pl_m_sample),x,pl_m_sample)== 0
       })
-      unlist(lapply(undiscovered, function(x){1-prod(1-(pl_m_sample[x]))}))
+
+      pl_m_post_undiscovered = lapply(undiscovered, function(x) {
+        if (sum(x)>0) {
+        arms(sum(x), log_pdf, lower = 0, upper = 1,
+             arguments = list(n = n, mu  = mu_s2_sample[1], s2= mu_s2_sample[1]),metropolis = FALSE)
+        } else {
+          0
+        }
+      })
+
+      unlist(lapply(pl_m_post_undiscovered, function(x) {1 - prod(1-x)}))
     }
   })
 }
@@ -73,6 +86,29 @@ multinom_sample <- function(prepost_pl_nc, prepost_pl_c, N){
 }
 
 
+#' @title TO DO multinom_sample
+#'
+#' @description TO DO multinom_sample
+#'
+#' @param prepost_pl_nc TO DO
+#' @param prepost_pl_c TO DO
+#' @param N TO DO
+#'
+#' @return
+#' @export multinom_sample
+#'
+
+multinom_expectation <- function(prepost_pl_nc, prepost_pl_c, N){
+  lapply(1:nrow(prepost_pl_nc), function(i) {
+    proba <- data.frame(nc = prepost_pl_nc[i,], c = prepost_pl_c[i,])
+    t(apply(X = proba, 1, FUN = function(x) {
+      N*c(probok = (1 - x["c"])*(1 - x["nc"]), probc = x["c"],
+        probnc = x["nc"] * (1 - x["c"]))
+    }))
+  })
+}
+
+
 #' @title TO DO prepost_analysis
 #'
 #' @description TO DO prepost_analysis
@@ -93,15 +129,18 @@ prepost_analysis <- function(fit_nc, fit_c, sample_size, n_end_users, sim){
   j_nc <- min(pm[pm$px_nc>-Inf,"m_nc"])
   j_c <- min(pm[pm$px_nc>-Inf,"m_c"])
   pl_nc <- short_pl(posterior_pl = fit_nc$posterior_pl, j = dm_nc$j)
+  mu_s2_nc <- fit_nc$posterior_mu_s2
   pl_c <- short_pl(posterior_pl = fit_c$posterior_pl, j = dm_c$j)
+  mu_s2_c <- fit_c$posterior_mu_s2
   s_pm <- sample_pm(pm, sim)
-    res_prepost_nc <- sapply(1:length(s_pm$m_nc), function(x){
-    prepost_pl(m = s_pm$m_nc[x], pl = pl_nc, n = sample_size)
+  res_prepost_nc <- sapply(1:length(s_pm$m_nc), function(x){
+    prepost_pl(m = s_pm$m_nc[x], pl = pl_nc, mu_s2 = mu_s2_nc, n = sample_size)
   })
-    res_prepost_c <- sapply(1:length(s_pm$m_c), function(x){
-    prepost_pl(m = s_pm$m_c[x], pl = pl_c, n = sample_size)
+  res_prepost_c <- sapply(1:length(s_pm$m_c), function(x){
+    prepost_pl(m = s_pm$m_c[x], pl = pl_c, mu_s2 = mu_s2_c, n = sample_size)
   })
-    y <- multinom_sample(prepost_pl_nc = res_prepost_nc,
+  # According to the use case replace multinom_sample by multinom_expectation
+  y <- multinom_sample(prepost_pl_nc = res_prepost_nc,
                      prepost_pl_c = res_prepost_c,
                      N = n_end_users)
   return(list(pm = pm, y = y))
