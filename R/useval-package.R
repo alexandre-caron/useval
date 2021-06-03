65f5bf40bb5100a9c07f66cb56f125eec4a0d012
Vincent Vandewalle <vincent.vandewalle@univ-lille.fr>
2021-06-03 15:55:48 +0200

    Add import

--- R/useval-package.R
+++ R/useval-package.R
@@ -10,6 +10,7 @@
 #' @import Rcpp
 #' @importFrom rstan sampling
 #' @importFrom bridgesampling bridge_sampler
+#' @importFrom stats rbinom rmultinom
 #'
 #' @references
 #' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.19.3. https://mc-stan.org
