829becae2fc41caa0b8af1ef087e8466270e4e7e
Vincent Vandewalle <vincent.vandewalle@univ-lille.fr>
2021-06-04 09:43:39 +0200

    Add of draft of the documentation of the dataset

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Nom du jeu de données
+#'
+#' Description un peu plus détaillées
+#'
+#' @format A logical matrix with 30 rows and 24 columns
+#' \describe{
+#'   The entry line i row j is equal to TRUE if user i has discovered problem
+#'   number j and 0 otherwise
+#' }
+#' @source \url{http://www.diamondse.info/}
+"expert_hf"
+
+#' Nom du jeu de données
+#'
+#' Description un peu plus détaillées
+#'
+#' @format A logical matrix with ... rows and ... columns
+#' \describe{
+#'   The entry line i row j is equal to TRUE if user i has discovered problem
+#'   number j and 0 otherwise
+#' }
+#' @source \url{http://www.diamondse.info/}
+"expert_lf"
+
+
+#' Nom du jeu de données
+#'
+#' Description un peu plus détaillées
+#'
+#' @format A logical matrix with ... rows and ... columns
+#' \describe{
+#'   The entry line i row j is equal to TRUE if user i has discovered problem
+#'   number j and 0 otherwise
+#' }
+#' @source \url{http://www.diamondse.info/}
+"naive_hf"
+
+
+#' Nom du jeu de données
+#'
+#' Description un peu plus détaillées
+#'
+#' @format A logical matrix with ... rows and ... columns
+#' \describe{
+#'   The entry line i row j is equal to TRUE if user i has discovered problem
+#'   number j and 0 otherwise
+#' }
+#' @source \url{http://www.diamondse.info/}
+"naive_lf"
